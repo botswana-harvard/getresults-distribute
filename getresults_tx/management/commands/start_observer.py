959b09b8eb48a32af2c8248072d48c60696aaca8
erik van widenfelt <ew2789@gmail.com>
2015-06-23 13:12:52 +0200

    add exception to management

--- getresults_tx/management/commands/start_observer.py
+++ getresults_tx/management/commands/start_observer.py
@@ -9,9 +9,11 @@
 
 import os
 import sys
+import time
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from paramiko import SSHException
 
 from getresults_tx.server import Server
 from getresults_tx.event_handlers import RemoteFolderEventHandler
@@ -27,16 +29,19 @@ class Command(BaseCommand):
         archive_dir = os.path.join(settings.MEDIA_ROOT, settings.GRTX_ARCHIVE_FOLDER)
         file_patterns = settings.GRTX_FILE_PATTERNS
         mime_types = settings.GRTX_MIME_TYPES
-        server = Server(
-            RemoteFolderEventHandler,
-            hostname=hostname,
-            source_dir=source_dir,
-            destination_dir=destination_dir,
-            archive_dir=archive_dir,
-            file_patterns=file_patterns,
-            mime_types=mime_types,
-            touch_existing=True,
-            mkdir_remote=True)
+        try:
+            server = Server(
+                RemoteFolderEventHandler,
+                hostname=hostname,
+                source_dir=source_dir,
+                destination_dir=destination_dir,
+                archive_dir=archive_dir,
+                file_patterns=file_patterns,
+                mime_types=mime_types,
+                touch_existing=True,
+                mkdir_remote=True)
+        except SSHException as e:
+            raise CommandError('SSHException: {}'.format(str(e)))
         sys.stdout.write('\n' + str(server) + '\n')
         sys.stdout.write('patterns: {}\n'.format(','.join([x for x in server.file_patterns])))
         sys.stdout.write('mime: {}\n'.format(','.join([x.decode() for x in server.mime_types])))
