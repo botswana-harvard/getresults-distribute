dfbdcd0aee15d8a85d99eed22dc9cbe581c5f8d9
erik van widenfelt <ew2789@gmail.com>
2015-06-29 16:28:56 +0200

    wsgi

--- getresults_dst/wsgi.py
+++ getresults_dst/wsgi.py
@@ -1,12 +1,12 @@
 import os
-import sys
+# import sys
 
 # sys.path.append(os.path.expanduser('~/source/getresults-distribute'))
 # sys.path.append(os.path.expanduser('~/source/getresults-distribute/getresults_dst'))
 # sys.path.append(os.path.expanduser('~/.virtualenvs/django18/lib/python3.4/site-packages'))
-# 
+#
 # activate_env = os.path.join(os.path.expanduser('~/.virtualenvs/django18/bin/activate_this.py'))
-# 
+#
 # with open(activate_env) as f:
 #     code = compile(f.read(), activate_env, 'exec')
 #     exec(code, dict(__file__=activate_env))
